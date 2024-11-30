63c6f5cec27fb2cece68643d47a0ffbedd6b0925
Mindula Madhuhansa <mimmindula@gmail.com>
2024-11-30 13:43:32 +0530

    add fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // add firebase config here
+  apiKey: "AIzaSyC-9-bY326xHwfDU-x_ygUqaTDNzxD4Y6U",
+  authDomain: "scoreboard-app-14012024.firebaseapp.com",
+  projectId: "scoreboard-app-14012024",
+  storageBucket: "scoreboard-app-14012024.appspot.com",
+  messagingSenderId: "754915355073",
+  appId: "1:754915355073:web:da6461d1a2df76a8b2a740",
+  measurementId: "G-074TM7Y0JY",
 };
 
 const app = initializeApp(firebaseConfig);
